65252b8e54893219aaca2c36839ff39b0109f5f0
Shireesha Govindu <shireeshagovindu24@gmail.com>
2026-01-05 05:59:28 +0530

    pluggedin the real LLM - OPEN AI API

--- src/generate.py
+++ src/generate.py
@@ -1,10 +1,25 @@
 # Using retreived chunks to answer the question - Augumented Generation
-# Mocking the LLM for now
+# Plugging in the real OPENAI LLM
+
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+# Loading the env variable
+load_dotenv()
+
+# Create OPENAI Client
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+print("OPENAI_API_KEY loaded:", os.getenv("OPENAI_API_KEY"))
+
 def generate_answer(context_chunks, question):
     context = "\n\n".join(context_chunks)
 
     prompt = f"""
-Use the context below to answer the question.
+You are an helpful assistant.
+Answer the question using ONLY the context below.
+If the answe is not present in the context, say "I don't know
 
 Context:
 {context}
@@ -12,4 +27,11 @@ Context:
 Question:
 {question}
 """
-    return prompt
+    response = client.chat.completions.create(
+        model= "gpt-5.2",
+        messages = [
+            {"role":"user", "content":prompt}
+        ],
+        temperature=0
+    )
+    return response.choices[0].message.content
